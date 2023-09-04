5b0c2ad327053ddf9c0384696b7c71f4b2646613
André Ricardo Morato Rosa <65589462+dedsdead@users.noreply.github.com>
2023-09-04 15:20:11 -0300

    endereço ok

--- src/main/java/sisgerim/backend/repositories/EnderecoRepository.java
+++ src/main/java/sisgerim/backend/repositories/EnderecoRepository.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sisgerim.backend.domain.endereco.Endereco;
 
-
 public interface EnderecoRepository extends JpaRepository<Endereco, UUID>{
     Optional<Endereco> findById(UUID id);
 }
