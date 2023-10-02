8abb6178824c44f31300f4055e186a6f584c83f4
André Ricardo Morato Rosa <65589462+dedsdead@users.noreply.github.com>
2023-10-02 20:08:15 -0300

    fix: Endereco domain

--- src/main/java/sisgerim/backend/domain/endereco/EnderecoRequestDTO.java
+++ src/main/java/sisgerim/backend/domain/endereco/EnderecoRequestDTO.java
@@ -3,4 +3,13 @@ package sisgerim.backend.domain.endereco;
 import java.util.UUID;
 import jakarta.validation.constraints.NotBlank;
 
-public record EnderecoRequestDTO(UUID id, @NotBlank String cep, @NotBlank String uf, @NotBlank String localidade, @NotBlank String bairro, @NotBlank String logradouro, int numero, String complemento) {}
+public record EnderecoRequestDTO(
+    UUID id,
+    @NotBlank String cep,
+    @NotBlank String uf,
+    @NotBlank String localidade,
+    @NotBlank String bairro,
+    @NotBlank String logradouro,
+    int numero,
+    String complemento
+) {}
